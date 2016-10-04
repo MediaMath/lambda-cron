780e691394a2731987174eea520dcff8f0da16e4
Javier Bravo <javibravo85@gmail.com>
2016-10-04 13:49:23 +0100

    Parametrized period and tests

--- main.py
+++ main.py
@@ -12,29 +12,40 @@ from croniter import croniter
 from datetime import datetime, timedelta
 
 
-PERIOD = timedelta(hours=1)
 BUCKET = "lambdacron-taskstoragebucket-1dnoooztm6rn0"
 QUEUE  = "LambdaCron-TaskQueue-3WCGEQEF3BCA"
 
-def start_of_period(timestamp):
-    """ Find the start of the current period
-    Arguments:
-    timestamp - ISO 8601 stamp string
 
-    Retruns:
-    Datetime objects for 1 second the start current evaluation period
-    """
-    actual_time = datetime.strptime(timestamp, "%Y-%m-%dT%H:%M:%SZ")
-    ideal_time = actual_time.replace(second=0)
-    before_period = ideal_time - timedelta(seconds=1)
-    return before_period
+class CronChecker:
 
+    def __init__(self, current_timestamp, hour_period=1, minutes_period=0):
+        self.start_of_period = self.get_start_of_period(current_timestamp)
+        self.period = timedelta(hours=hour_period, minutes=minutes_period)
+
+    def get_start_of_period(self, timestamp):
+        """ Find the start of the current period
+        Arguments:
+        timestamp - ISO 8601 stamp string
+
+        Retruns:
+        Datetime objects for 1 second the start current evaluation period
+        """
+        actual_time = datetime.strptime(timestamp, "%Y-%m-%dT%H:%M:%SZ")
+        ideal_time = actual_time.replace(second=0)
+        before_period = ideal_time - timedelta(seconds=1)
+        return before_period
+
+    def should_run(self, cron_expression):
+        next_event = croniter(cron_expression, self.start_of_period).get_next(datetime)
+        if self.start_of_period < next_event and next_event <= (self.start_of_period + self.period):
+            return True
+        return False
 
 
 def handler(event, _):
     """ Main function """
     print(event)
-    start = start_of_period(event['time'])
+    cron_checker = CronChecker(event['time'], hour_period=1, minutes_period=0)
 
     s3 = boto3.resource('s3')
     bucket = s3.Bucket(BUCKET)
@@ -45,9 +56,7 @@ def handler(event, _):
     for obj in bucket.objects.all():
         task = yaml.load(obj.get()['Body'].read())
         print(task)
-        next_event = croniter(task['expression'], start).get_next(datetime)
-        print(next_event)
-        if start < next_event and next_event <= start + PERIOD:
+        if cron_checker.should_run(task['expression']):
             print("{} fired".format(task['name']))
             queue.send_message(MessageBody="{} fired".format(task['name']))
 
---
+++ tests/test_cron_checker.py
@@ -0,0 +1,35 @@
+import pytest
+from datetime import datetime
+from main import CronChecker
+
+def test_should_run_basic():
+    cron_checker = CronChecker('2016-10-01T09:00:00Z')
+    assert cron_checker.should_run('0 9 * * *') == True
+
+def test_should_not_run_basic():
+    cron_checker = CronChecker('2016-10-01T09:00:00Z')
+    assert cron_checker.should_run('0 10 * * *') == False
+
+def test_period_1_hour_should_run_min_30():
+    cron_checker = CronChecker('2016-10-01T09:00:00Z')
+    assert cron_checker.should_run('30 9 * * *') == True
+
+def test_period_30_min_should_not_run_min_30():
+    cron_checker = CronChecker('2016-10-01T09:00:00Z', 0, 30)
+    assert cron_checker.should_run('30 9 * * *') == False
+
+def test_period_5_min_should_run_min_04():
+    cron_checker = CronChecker('2016-10-01T09:00:00Z', 0, 5)
+    assert cron_checker.should_run('4 9 * * *') == True
+
+def test_period_5_min_should_not_run_min_05():
+    cron_checker = CronChecker('2016-10-01T09:00:00Z', 0, 5)
+    assert cron_checker.should_run('5 9 * * *') == False
+
+def test_period_5_min_should_run_before_the_time_less_than_5_minutes():
+    cron_checker = CronChecker('2016-10-01T08:58:59Z', 0, 5)
+    assert cron_checker.should_run('0 9 * * *') == True
+
+def test_period_5_min_should_not_run_before_the_time_more_or_equal_5_minutes():
+    cron_checker = CronChecker('2016-10-01T08:55:59Z', 0, 5)
+    assert cron_checker.should_run('0 9 * * *') == False
