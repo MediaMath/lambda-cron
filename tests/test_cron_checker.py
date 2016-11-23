fce91b5db69d094940d4f538399457a7a87a9eb0
Javier Bravo <javibravo85@gmail.com>
2016-11-23 16:47:57 -0500

    Adding tests for CronChecker

--- tests/test_cron_checker.py
+++ tests/test_cron_checker.py
@@ -39,3 +39,18 @@ def test_period_5_min_should_run_before_the_time_less_than_5_minutes():
 def test_period_5_min_should_not_run_before_the_time_more_or_equal_5_minutes():
     cron_checker = CronChecker('2016-10-01T08:55:59Z', 0, 5)
     assert cron_checker.should_run('0 9 * * *') == False
+
+
+def test_period_2_hours_should_run_for_less_2_hours_ahead_process():
+    cron_checker = CronChecker('2016-10-01T08:55:59Z', 2, 0)
+    assert cron_checker.should_run('0 10 * * *') == True
+
+
+def test_period_2_hours_should_run_for_less_2_hours_ahead_process_II():
+    cron_checker = CronChecker('2016-10-01T08:55:59Z', 2, 0)
+    assert cron_checker.should_run('54 10 * * *') == True
+
+
+def test_period_2_hours_should_not_run_for_more_than_2_hours_ahead_process():
+    cron_checker = CronChecker('2016-10-01T08:55:59Z', 2, 0)
+    assert cron_checker.should_run('56 10 * * *') == False
