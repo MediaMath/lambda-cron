c49ab133c0eb38d216a18e2cc61748d1ba704c1b
Javier Bravo <javibravo85@gmail.com>
2016-11-23 18:17:10 -0500

    Add frequency settings and fix alarm settings

--- cli/config_cli.py
+++ cli/config_cli.py
@@ -26,16 +26,22 @@ class ConfigCli:
         self.environment = environment
         self.bucket = DEFAULT_BUCKET_PATTERN.format(environment=self.environment)
         self.alarm = False
+        self.hours = 1
+        self.minutes = False
 
         self.config = load_config()
         self.set_bucket()
         self.set_alarm()
+        self.set_frequency()
 
     def set_alarm(self):
         if self.config and (self.environment in self.config) and ('alarm' in self.config[self.environment]):
             self.alarm = self.config[self.environment]['alarm']
         elif self.config and ('all' in self.config) and ('alarm' in self.config['all']):
             self.alarm = self.config['all']['alarm']
+
+        if not isinstance(self.alarm, bool):
+            raise Exception("Settings from 'alarm' must be a bool value")
 
     def set_bucket(self):
         if self.config and (self.environment in self.config) and ('bucket' in self.config[self.environment]):
@@ -46,4 +52,22 @@ class ConfigCli:
             else:
                 self.bucket = self.config['all']['bucket'] + "-{}".format(self.environment)
 
+    def set_frequency(self):
+        config_every = None
+        if self.config and (self.environment in self.config) and ('every' in self.config[self.environment]):
+            config_every = self.config[self.environment]['every']
+        elif self.config and ('all' in self.config) and ('every' in self.config['all']):
+            config_every = self.config['all']['every']
 
+        if not config_every:
+            return
+
+        if ('hours' in config_every) and ('minutes' in config_every):
+            raise Exception('Only one of hours or minutes must be used for the frequency. Both are not allowed.')
+
+        if 'hours' in config_every:
+            self.hours = config_every['hours']
+            self.minutes = False
+        if 'minutes' in config_every:
+            self.minutes = config_every['minutes']
+            self.hours = False
--- tests/test_config_cli.py
+++ tests/test_config_cli.py
@@ -16,6 +16,8 @@ def test_should_use_custom_for_environment(monkeypatch):
     config = cli.config_cli.ConfigCli('prod')
     assert config.bucket == 'test-bucket-custom'
     assert config.alarm
+    assert config.minutes == 5
+    assert config.hours == False
 
 
 def test_should_use_for_all_environment(monkeypatch):
@@ -23,13 +25,17 @@ def test_should_use_for_all_environment(monkeypatch):
     config = cli.config_cli.ConfigCli('other')
     assert config.bucket == 'test-bucket-all-other'
     assert config.alarm == False
+    assert config.minutes == False
+    assert config.hours == 2
 
 
-def test_should_use_for_all_environment_2(monkeypatch):
+def test_should_use_custom_and_all_environment_2(monkeypatch):
     monkeypatch.setattr(cli.config_cli, 'get_cli_config_file_path', valid_cong_file_path)
     config = cli.config_cli.ConfigCli('staging')
     assert config.bucket == 'test-bucket-all-staging'
     assert config.alarm == False
+    assert config.minutes == False
+    assert config.hours == 2
 
 
 def test_should_use_global_default_settings(monkeypatch):
@@ -37,9 +43,20 @@ def test_should_use_global_default_settings(monkeypatch):
     config = cli.config_cli.ConfigCli('test')
     assert config.bucket == 'LambdaCron-test'
     assert config.alarm == False
+    assert config.minutes == False
+    assert config.hours == 1
 
 
 def test_alarm_should_be_false_for_invalid_values(monkeypatch):
-    monkeypatch.setattr(cli.config_cli, 'get_cli_config_file_path', invalid_config_file_path)
-    config = cli.config_cli.ConfigCli('alarm_bad_value')
-    assert config.alarm == False
+    monkeypatch.setattr(cli.config_cli, 'get_cli_config_file_path', valid_cong_file_path)
+    with pytest.raises(Exception) as exception_info:
+        cli.config_cli.ConfigCli('alarm_bad_value')
+    assert "Settings from 'alarm' must be a bool value" in str(exception_info.value)
+
+
+def test_frequency_raise_exeption_using_hours_and_minutes(monkeypatch):
+    monkeypatch.setattr(cli.config_cli, 'get_cli_config_file_path', valid_cong_file_path)
+    with pytest.raises(Exception) as exception_info:
+        cli.config_cli.ConfigCli('frequency_error')
+    assert 'Only one of hours or minutes must be used for the frequency. Both are not allowed.' in str(exception_info.value)
+
