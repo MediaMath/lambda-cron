643881d585042cc7d4eb32601adfb665465bf094
Javier Bravo <javibravo85@gmail.com>
2017-02-26 19:56:33 +0000

    Add license header to all files

--- lambda_cron/aws/main.py
+++ lambda_cron/aws/main.py
@@ -1,8 +1,16 @@
-""" Cron Lambda function
-    This lambda function is designed to be executed periodically,
-    read task definitions from a S3 bucket and if the cron expression
-    in the tasks indicates is should be executed send it to an SQS queue.
-"""
+# Copyright (C) 2016 MediaMath <http://www.mediamath.com>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 import logging
 import boto3
